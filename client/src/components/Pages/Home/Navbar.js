29b9b1babef403f27a7b7b371ba69775dca86bff
Christopher Martin <chmartin@et.esiea.fr>
2019-09-30 11:38:37 +0200

    Navlink setup + Signup component
    Put navlink in singup button
    Wrote signup component

--- client/src/components/Pages/Home/Navbar.js
+++ client/src/components/Pages/Home/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
+
 import '../../../assets/style/css/navbar.min.css'
+import logo from '../../../assets/images/logo.png'
 
 const Navbar = () => {
     return (
             <div className="navbar">
+                <div className="logo-wrapper">
+                    <img src={logo} alt="logo"/>
+                </div>
                 <div className="button-wrapper">
                     <div className="button-inner">
                         <button className="button-login">
@@ -12,7 +18,7 @@ const Navbar = () => {
                     </div>
                     <div className="button-inner">
                         <button className="button-signup">
-                            INSCRIPTION
+                            <NavLink to='/signup' className="inner-link">INSCRIPTION</NavLink>
                         </button>
                     </div>
                 </div>
---
+++ client/src/components/Pages/Signup/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import {NavLink} from "react-router-dom";
+
+const Signup = () => (
+    <div id='signup'>
+        Signup
+        <NavLink to='/'>
+            back home
+        </NavLink>
+    </div>
+);
+
+export default Signup
