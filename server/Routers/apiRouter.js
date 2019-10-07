e58b8ac4ee27dd35ccc038fe6f331b1c12fa6b49
Christopher Martin <chmartin@et.esiea.fr>
2019-10-07 15:59:42 +0200

    mysql connection configuration
    Create config file and database class to help connection to mysql

--- server/Routers/apiRouter.js
+++ server/Routers/apiRouter.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
+import database from "../mysqlDatabase";
 
 const apiRouter = Router();
+const db = new database();
 
 apiRouter.get('/users', (req, res) => {
-    res.json({
-        data: "COUCOU ! "
-    })
+    db.query("SELECT * FROM users", [], (data) => {
+        console.log(data);
+        res.json(data);
+    });
 });
 
 export default apiRouter;
