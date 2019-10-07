91a71fe1249294a4e16497b372428ee8b9390d4f
Christopher Martin <chmartin@et.esiea.fr>
2019-10-07 15:11:10 +0200

    env vars setup
    installation of cors and dotenv packages

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
-const express = require("express");
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
 const app = express();
-
-app.get("/api/test", (req,res) => {
-    res.render("index.ejs");
-}).listen(3001);
+app.use(cors());
+app.use(express.json());
 
 
+
+app.listen(process.env.PORT || 5000,() => console.log(`Server is listening on port ${process.env.PORT}`));
+
+export default app;
